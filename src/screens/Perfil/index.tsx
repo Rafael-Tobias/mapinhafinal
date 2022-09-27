e1c4e4146761accf83f44729fbbb34defc8be9c0
TobiasMaugus <tobiasmaugus705@gmail.com>
2022-09-27 09:58:01 -0300

    expo notification

--- src/screens/Perfil/index.tsx
+++ src/screens/Perfil/index.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { View, Text, ImageBackground, Image } from "react-native";
 import CardSocial from "../../components/CardSocial";
 import { FontAwesome5 } from "@expo/vector-icons";
 import styles from "./styles";
 import Button from "../../components/Button";
 import { useAuth } from "../../hook/auth";
+import * as Notifications from "expo-notifications";
+import { registerForPushNotificationsAsync } from "../../services/data/Push";
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function Perfil() {
   const { user } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (user&& user.profile_photo_url) {
+      setIsLoading(false);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    async function fetchToken() {
+      const token = await registerForPushNotificationsAsync()
+      console.log(token)
+    }
+    fetchToken()
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={{ uri: user?.profile_photo_url }} style={styles.img}/>
